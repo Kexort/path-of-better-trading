1b43036a480c35c8581d143c5e61212530c1dd24
Kexort <kexort@gmail.com>
2022-03-01 10:33:07 +0100

    Add parsing logic

--- src/client/App.js
+++ src/client/App.js
@@ -1,23 +1,64 @@
 import React, { Component } from 'react';
 import './app.css';
-import ReactImage from './react.png';
+import LoadingButton from '@mui/lab/LoadingButton';
+import TextField from '@mui/material/TextField';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+
+const theme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+});
 
 export default class App extends Component {
-  state = { username: null };
+  state = { loading: false, text: "https://pastebin.com/WhWjXGga", build: null }
 
   componentDidMount() {
-    fetch('/api/getUsername')
-      .then(res => res.json())
-      .then(user => this.setState({ username: user.username }));
+  }
+
+  handleChange = (event) => {
+    this.setState({
+      text: event.target.value,
+    });
+  };
+
+  handleClick = () => {
+    this.setState({ loading: !this.state.loading });
+    this.fetchAPI(this.state.text);
+  }
+
+  fetchAPI(param) {
+    fetch('api/parse?paste=' + param).then(response => response.json()
+      .then((data) => {
+        this.setState({ build: data, loading: false });
+      }));
   }
 
   render() {
-    const { username } = this.state;
     return (
-      <div>
-        {username ? <h1>{`Hello ${username}`}</h1> : <h1>Loading.. please wait!</h1>}
-        <img src={ReactImage} alt="react" />
-      </div>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <div>
+          <br />
+          <br />
+          <TextField sx={{ width: '40%' }} id="outlined-basic" label="Pastebin" variant="outlined" onChange={this.handleChange} value={this.state.text} />
+          <br />
+          <br />
+          {/* <Button onClick={this.buttonHandler} variant="contained">Let&apos;s go!</Button> */}
+          <LoadingButton
+            onClick={this.handleClick}
+            loading={this.state.loading}
+            loadingIndicator="Loading..."
+            variant="outlined"
+          >
+            Let&apos;s go!
+          </LoadingButton>
+          <br />
+          <br />
+          {JSON.stringify(this.state.build?.parsed.PathOfBuilding.Items.Item)}
+        </div>
+      </ThemeProvider>
     );
   }
 }
--- src/server/index.js
+++ src/server/index.js
@@ -1,9 +1,41 @@
 const express = require('express');
-const os = require('os');
+const axios = require('axios');
+const zlib = require('zlib');
+const convert = require('xml-js');
 
 const app = express();
 
+const testdata = { items: [{ name: 'TESTITEM' }] };
+
+function getPastebinData(url) {
+  return axios.get(url).then(res => res.data);
+}
+
+function parsePastebin(data) {
+  return zlib.inflateSync(
+    Buffer.from(
+      data.replace('-', '+').replace('_', '/'), 'base64'
+    )
+  );
+}
+
+async function parseContent(data) {
+  // https://pastebin.com/WhWjXGga
+  // https://pastebin.com/raw/WhWjXGga
+  console.log(data);
+  if (data.startsWith('https://pastebin.com')) {
+    const url = 'https://pastebin.com/raw/'.concat(data.split('/').at(-1));
+    const content = await getPastebinData(url);
+    const parsedContent = JSON.parse(convert.xml2json(parsePastebin(content), { compact: true }));
+    return { parsed: parsedContent };
+  }
+  return { result: 'no result' };
+}
+
 app.use(express.static('dist'));
-app.get('/api/getUsername', (req, res) => res.send({ username: os.userInfo().username }));
+app.get('/api/parseTest', (req, res) => res.send(testdata));
+app.get('/api/parse', async (req, res) => {
+  res.send(await parseContent(req.query.paste));
+});
 
 app.listen(process.env.PORT || 8080, () => console.log(`Listening on port ${process.env.PORT || 8080}!`));
